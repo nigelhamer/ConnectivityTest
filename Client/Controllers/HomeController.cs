e2f43bd1eb1bd39d693dfa2c2ba61a6a558fba74
Nigel Hamer <nigel@hamersmith.co.uk>
2017-06-28 16:57:09 +0100

    Initial Commit

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> Index()
+        {
+            string name = System.Configuration.ConfigurationManager.AppSettings["name"];
+            ViewBag.Message = await GetServerResponseAsync(name);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        static async Task<string> GetServerResponseAsync(string name)
+        {
+            string baseApiUrl = System.Configuration.ConfigurationManager.AppSettings["baseApiUrl"];
+
+            client.BaseAddress = new Uri(baseApiUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                var serverResponse = "";
+                var uri = new Uri(client.BaseAddress, $"api/helloworld?name={name}");
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    serverResponse = await response.Content.ReadAsAsync<string>();
+                }
+                return serverResponse;
+            }
+            catch (Exception ex)
+            {
+                return $"Unable to get message from server. Error was {ex}";
+            }
+
+            
+        }
+    }
+}
---
+++ Server/HelloWorldController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Http;
+
+namespace Server
+{
+    public class HelloWorldController : ApiController
+    {
+        // GET: api/HelloWorld
+        public IHttpActionResult Get(string name)
+        {           
+            return Ok($"Hello world {name}, from  the server. The IP of the caller was {GetClientIp()}. My IP is {GetLocalIPAddress()}");
+        }
+
+        private string GetClientIp(HttpRequestMessage request = null)
+        {
+            request = request ?? Request;
+
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            }            
+            else if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
